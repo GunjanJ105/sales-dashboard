cc1234f5168e890281fe6e8266a5281a9e739cd0
GunjanJ105 <jgunjan388@gmail.com>
2026-05-24 17:32:13 +0530

    add sqlite database

--- utils.py
+++ utils.py
@@ -1,8 +1,18 @@
 import pandas as pd
+import sqlite3
 
 def load_data():
-    df = pd.read_csv("data/Sample - Superstore.csv", encoding="latin1")  # ← add encoding
+    # Load CSV
+    df = pd.read_csv("data/Sample - Superstore.csv", encoding="latin1")
     df["Order Date"] = pd.to_datetime(df["Order Date"])
     df["Year"] = df["Order Date"].dt.year
     df["Month"] = df["Order Date"].dt.month_name()
+
+    # Save to SQLite database
+    conn = sqlite3.connect("sales.db")
+    df.to_sql("sales", conn, if_exists="replace", index=False)
+
+    # Read back from SQL
+    df = pd.read_sql("SELECT * FROM sales", conn)
+    conn.close()
     return df
